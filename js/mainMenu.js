d259809893335366a54800b380b5830973215330
Andrés <demonykira@gmail.com>
2016-05-28 15:23:03 -0500

    1
    primera entrega

---
+++ js/mainMenu.js
@@ -0,0 +1,22 @@
+$(document).ready(mainMenu);
+
+var contador = 1;
+
+function mainMenu(){
+    $('div.menu_bar').click(function(){
+        if(contador==1){
+            $('nav').animate({
+
+            	left: '0'
+            });
+            contador = 0;
+        }else{
+               $('nav').animate({
+            	left: '-100%'
+            });
+            contador = 1;
+        }
+
+    });
+
+}
